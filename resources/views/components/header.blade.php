d93d89f1c52e9f3117e9907c7d92c4870896739a
ShabbirI123 <shabbirislam801@gmail.com>
2021-09-22 19:32:33 +0200

    Added new navigation bar with css

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,30 +1,16 @@
 <link href="{{asset("css/component.css")}}" rel="stylesheet">
-<script src="{{asset("js/index.js")}}" type="text/javascript"></script>
-{{--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"--}}
-{{--      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">--}}
-<script src="/js/app.js"></script>
-<link rel="stylesheet" href={{ asset('css/app.css') }}>
-<div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Jumo Sensor</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/about">About Us</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <a class="btn btn-primary" href="/login" role="button">Sign In</a>
-            </form>
-        </div>
+<script src="https://kit.fontawesome.com/dd2365140c.js" crossorigin="anonymous"></script>
+<div id="navBar">
+    <nav>
+        <input type="checkbox" id="checkbox">
+        <label for="checkbox" class="checkBtn">
+            <i class="fas fa-bars" aria-hidden="true"></i>
+        </label>
+        <label class="logo">Jumo Sensor</label>
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ABOUT US</a></li>
+            <li><a href="#">SIGN IN</a></li>
+        </ul>
     </nav>
-</div>
 </div>
