6145435e0a7b0daf8fd6e69bd5a094666c454703
ShabbirI123 <shabbirislam801@gmail.com>
2022-02-04 18:13:06 +0100

    updated header, footer and the landing page

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,51 @@
 <link href="{{asset('css/component.css')}}" rel="stylesheet">
 <div>
-    <footer class="footer">
+    <footer class="footer-distributed">
+
+        <div class="footer-left">
+
+            <h3>Jumo<span>Sensor</span></h3>
+
+            <p class="footer-links">
+                <a href="/" class="link-1">Home</a>
+                <a href="/about">About Us</a>
+                <a href="/login">Sign In</a>
+            </p>
+
+            <p class="footer-company-name">Jumo Sensor © 2022</p>
+        </div>
+
+        <div class="footer-center">
+
+            <div>
+                <i class="fa fa-map-marker"></i>
+                  <p><a href="https://www.jumo.at/">JUMO Mess- und Regelgeräte GmbH</a></p>
+            </div>
+
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>Telefon: +43 1 61061-0</p>
+            </div>
+
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p>E-Mail: <a href="mailto:info.at@jumo.net">info.at@jumo.net</a> </p>
+            </div>
+
+        </div>
+
+        <div class="footer-right">
+
+            <p class="footer-company-about">
+                <span>About the company</span>
+                Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+            </p>
+
+
+        </div>
+
+    </footer>
+    <!--<footer class="footer">
         <div class="container-fluid">
             <div id="footer-content">
                 <section class="footer-header" id="link">
@@ -31,5 +76,5 @@
                 </section>
             </div>
         </div>
-    </footer>
+    </footer>-->
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,8 @@
 
     <title>Laravel</title>
 
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
     <link href="{{asset('css/index.css')}}" rel="stylesheet">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -13,27 +15,73 @@
 </head>
 <body>
 <x-header></x-header>
-
-<main id="main">
-   <section class="main">
-       <section id="welcome">
-           <div class="content">
-               <h1>Jumo Sensor<br><span>We digitize data</span></h1>
-           </div>
-       </section>
-       <section id="info">
-            <div class="content">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-            rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
-            sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-            aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-            gubergssren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </div>
-           <div>Test</div>
-       </section>
-   </section>
-</main>
-
+<div id="welcome">
+    <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> We're thrilled to have you here! Get ready to dive into your new account. </div>
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <!-- LOGO -->
+        <tr>
+            <td bgcolor="#203367" align="center">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                    <tr>
+                        <td align="center" valign="top" style="padding: 40px 10px 40px 10px;"> </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#203367" align="center" style="padding: 0px 10px 0px 10px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                    <tr>
+                        <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
+                            <h1 style="font-size: 48px; font-weight: 400; margin: 2px;">Welcome!</h1> <img src="https://img.icons8.com/wired/64/000000/sensor.png" width="100" height="100" style="display: block; border: 0px;" />
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                    <tr>
+                        <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                            <p style="margin: 0;">We're excited to have you get started. Please get in contact with us to set up your account.</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 0px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                            <p style="margin: 0;">To get in touch with us, please send a mail to: <br><b>Jumo_weather@protonmail.com</b><br></p>
+                        </td>
+                        <br>
+                    </tr> <!-- COPY -->
+                    <tr>
+                        <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 20px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                            <p style="margin: 0;">If you have any questions, we're always happy to help out.</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                            <p style="margin: 0;">Cheers,<br>Your JUMO Team</p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#f4f4f4" align="center" style="padding: 30px 10px 0px 10px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                    <tr>
+                        <td bgcolor="#FFECD1" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                            <h2 style="font-size: 20px; font-weight: 400; color: #111111; margin: 0;">Need more help?</h2>
+                            <p style="margin: 0;"><a href="https://www.jumo.at/web/about-us/career-at/contact-at" target="_blank" style="color: #203367;">We&rsquo;re here to help you out</a></p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+    <br>
+    <br>
+</div>
 <x-footer></x-footer>
 </body>
 </html>
